93ae462e065bcbc0164c1e667466578664c62ad3
unknown <linusolanderpaulsson@gmail.com>
2020-08-31 17:50:40 +0200

    Use new cors proxy to not have to bother with slow connections due to rate limiting

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 const API_URL = "https://roleplay.co.uk/rpukapi/capture";
-const CORS_PROXY_URL = "https://cors-anywhere.herokuapp.com";
+const CORS_PROXY_URL = "https://cors.linusop.workers.dev";
 
 export default async function getApiData() {
-  const response = await fetch(`${CORS_PROXY_URL}/${API_URL}`);
+  const response = await fetch(`${CORS_PROXY_URL}/?${API_URL}`);
   return response.json();
 }
