592cb3bc7fc2eb10552dda45ac3fceefaf02cf33
unknown <linusolanderpaulsson@gmail.com>
2020-08-31 21:00:55 +0200

    More linter problems, gosh darn linebreaks

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,6 @@ module.exports = {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
     quotes: ["error", "double", { allowTemplateLiterals: true }],
-    "linebreak-style": ["error", "windows"],
+    "linebreak-style": "off",
   },
 };
